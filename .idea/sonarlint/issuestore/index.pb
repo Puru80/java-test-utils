329a9e1c257be5c9796f3ab22d111024a1fe8acf
Puru80 <puruagarwal@flash.tech>
2022-12-01 16:47:22 +0530

    java maze le raha hai BO

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-S
-#src/main/java/com/example/Test.java,1/8/18d550632c9ba400a158d31d24d2bcb422ba9650
--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -1,11 +1,36 @@
 package com.example;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.UUID;
 
 public class Test {
 
     public static void main(String[] args) {
-        String value = String.format("/close?requestId=%s", UUID.randomUUID().toString());
-        System.out.println(value);
+        Integer i = 5;
+        Set<String> possibleStrings = new LinkedHashSet<>();
+        possibleStrings.add("emailId");
+
+        updateInt(i, possibleStrings);
+
+//        for (String val : possibleStrings) {
+//            System.out.println(val);
+//        }
+//        System.out.println(possibleStrings.hashCode());
+
+
+        System.out.println(i.hashCode());
+    }
+
+
+    public static void updateInt(Integer i, Set<String> possibleStrings){
+        i = 1;
+        System.out.println(i.hashCode());
+//        possibleStrings.add("Hello World!!!!");
+
+//        System.out.println(possibleStrings.hashCode());
+//        possibleStrings = new LinkedHashSet<>();
+//        possibleStrings.add("updateInt(Integer i, Set<String> possibleStrings)");
+//        System.out.println(possibleStrings.hashCode());
     }
 }
