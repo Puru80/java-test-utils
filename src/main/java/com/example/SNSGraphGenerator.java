e2f4cb8917d2f32a4de08aea2233a69214322281
Puru80 <puru.agar99@gmail.com>
2023-01-06 13:42:02 +0530

    some more scripts

--- src/main/java/com/example/SNSGraphGenerator.java
+++ src/main/java/com/example/SNSGraphGenerator.java
@@ -4,26 +4,7 @@ public class SNSGraphGenerator {
 
     private static String topics = "";
 
-    private static String snsWidgets = "{\n" +
-            "            \"height\": 6,\n" +
-            "            \"width\": 6,\n" +
-            "            \"y\": 0,\n" +
-            "            \"x\": 6,\n" +
-            "            \"type\": \"metric\",\n" +
-            "            \"properties\": {\n" +
-            "                \"metrics\": [\n" +
-            "                    [ \"AWS/SNS\", \"NumberOfNotificationsDelivered\", \"TopicName\", \"%s\" ],\n" +
-            "                    [ \".\", \"NumberOfNotificationsFailed\", \".\", \".\" ],\n" +
-            "                    [ \".\", \"NumberOfMessagesPublished\", \".\", \".\" ]\n" +
-            "                ],\n" +
-            "                \"view\": \"timeSeries\",\n" +
-            "                \"stacked\": false,\n" +
-            "                \"region\": \"ap-south-1\",\n" +
-            "                \"stat\": \"Sum\",\n" +
-            "                \"period\": 60,\n" +
-            "                \"title\": \"%s\"\n" +
-            "            }\n" +
-            "        },";
+    private static String snsWidgets = ",";
 
     public static void main(String[] args) {
         String[] topicArr = topics.split("\n");
