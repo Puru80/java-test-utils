ac1d28911f4f9d37133af96360c35402f032cf62
Puru80 <puru.agar99@gmail.com>
2023-01-13 11:13:13 +0530

    sqsgraph generator updated

--- src/main/java/com/example/SQSGraphGenerator.java
+++ src/main/java/com/example/SQSGraphGenerator.java
@@ -1,16 +1,39 @@
 package com.example;
 
+import java.util.Queue;
+
 public class SQSGraphGenerator {
 
     public static String queueNames = "";
 
-    public static String sqsWidget = "";
+    //region sqsWidgets
+    public static String sqsWidget = "{\n" +
+            "            \"height\": 6,\n" +
+            "            \"width\": 6,\n" +
+            "            \"y\": 0,\n" +
+            "            \"x\": 0,\n" +
+            "            \"type\": \"metric\",\n" +
+            "            \"properties\": {\n" +
+            "                \"metrics\": [\n" +
+            "                    [ \"AWS/SQS\", \"NumberOfMessagesReceived\", \"QueueName\", \"queueName\" ],\n" +
+            "                    [ \".\", \"NumberOfMessagesDeleted\", \".\", \".\" ],\n" +
+            "                    [ \".\", \"NumberOfMessagesSent\", \".\", \".\" ]\n" +
+            "                ],\n" +
+            "                \"view\": \"timeSeries\",\n" +
+            "                \"stacked\": false,\n" +
+            "                \"region\": \"ap-south-1\",\n" +
+            "                \"stat\": \"Sum\",\n" +
+            "                \"period\": 60,\n" +
+            "                \"title\": \"queueName\"\n" +
+            "            }\n" +
+            "        }, ";
+    //endregion
 
     public static void main(String[] args) {
         String queueArr[] = queueNames.split("\n");
 
         for(String str: queueArr) {
-            System.out.println(String.format(sqsWidget, str, str));
+            System.out.println(sqsWidget.replace("queueName", str));
         }
 
         System.out.println();
